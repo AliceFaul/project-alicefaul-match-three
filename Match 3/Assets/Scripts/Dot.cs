71e247417e1b9a9cef096a425d3b0ae3b3b2ff71
33Bruh <maihieu6714@gmail.com>
2026-04-27 15:10:39 +0700

    Add swipe resist when touch to swipe dot in board

--- Match 3/Assets/Scripts/Dot.cs
+++ Match 3/Assets/Scripts/Dot.cs
@@ -21,7 +21,9 @@ public class Dot : MonoBehaviour {
     private Vector2 _firstTouchPosition;
     private Vector2 _finalTouchPosition;
     private Vector2 _tempPosition; // Temporary variable to store the position of the dot while it is moving
+    
     public float swipeAngle = 0;
+    public float swipeResist = 1f; // Minimum distance the swipe must cover to be registered as a valid swipe
 
     // Initialize
     private void Start() {
@@ -77,24 +79,25 @@ public class Dot : MonoBehaviour {
     private void OnMouseDown() {
         // Convert the mouse position to world coordinates and store it as the first touch position
         _firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // Debug.Log(_firstTouchPosition);
     }
 
     private void OnMouseUp() {
         // Convert the mouse position to world coordinates and store it as the final touch position
         _finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // Debug.Log(_finalTouchPosition);
         CalculateAngle();
     }
 
     private void CalculateAngle() {
-        // Calculate the angle of the swipe using the arctangent of the difference in
-        // y and x coordinates between the final and first touch positions,
-        // and convert it from radians to degrees
-        swipeAngle = Mathf.Atan2(_finalTouchPosition.y - _firstTouchPosition.y, 
-            _finalTouchPosition.x - _firstTouchPosition.x) * 180 / Mathf.PI;
-        Debug.Log(swipeAngle);
-        MovePieces(); // Call the MovePieces method to move the dots based on the calculated swipe angle
+        if (Mathf.Abs(_finalTouchPosition.y - _firstTouchPosition.y) > swipeResist ||
+            Mathf.Abs(_finalTouchPosition.x - _firstTouchPosition.x) > swipeResist)
+        {
+            // Calculate the angle of the swipe using the arctangent of the difference in
+            // y and x coordinates between the final and first touch positions,
+            // and convert it from radians to degrees
+            swipeAngle = Mathf.Atan2(_finalTouchPosition.y - _firstTouchPosition.y,
+                _finalTouchPosition.x - _firstTouchPosition.x) * 180 / Mathf.PI;
+            MovePieces(); // Call the MovePieces method to move the dots based on the calculated swipe angle
+        }
     }
 
     private void MovePieces() { 
