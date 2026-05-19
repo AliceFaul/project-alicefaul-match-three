c913a0092c1892f72d689a92c97cbd4fb2b42819
33Bruh <maihieu6714@gmail.com>
2026-05-19 19:49:09 +0700

    fix: Way to generate bomb
    Generate bomb by swipe angle of a current dot instead of random

--- Match 3/Assets/Scripts/MatchFinder.cs
+++ Match 3/Assets/Scripts/MatchFinder.cs
@@ -134,6 +134,7 @@ public class MatchFinder : MonoBehaviour {
             if(_board.currentDot.isMatched) {
                 _board.currentDot.isMatched = false;
                 int typeOfBomb = Random.Range(0, 99);
+                /*
                 if(typeOfBomb < 50) { 
                     // 50% chance to spawn a row bomb
                     _board.currentDot.MakeBomb(BombType.Row);
@@ -141,17 +142,35 @@ public class MatchFinder : MonoBehaviour {
                     // 50% chance to spawn a column bomb
                     _board.currentDot.MakeBomb(BombType.Column);
                 }
+                */
+                
+                if((_board.currentDot.swipeAngle > -45 && _board.currentDot.swipeAngle <= 45) || 
+                    (_board.currentDot.swipeAngle < -135 && _board.currentDot.swipeAngle >= 135)) 
+                { 
+                    _board.currentDot.MakeBomb(BombType.Row);
+                } else { 
+                    _board.currentDot.MakeBomb(BombType.Column);
+                }
             } else if(_board.currentDot._otherDot != null) {
                 // If the other piece is matched, make it a bomb
                 var otherDot = _board.currentDot._otherDot.GetComponent<Dot>();
                 if(otherDot != null) {
                     otherDot.isMatched = false;
+                    /*
                     int typeOfBomb = Random.Range(0, 99);
                     if(typeOfBomb < 50) { 
                         // 50% chance to spawn a row bomb
                         otherDot.MakeBomb(BombType.Row);
                     } else {
                         // 50% chance to spawn a column bomb
+                        otherDot.MakeBomb(BombType.Column);
+                    }
+                    */
+                    if((otherDot.swipeAngle > -45 && otherDot.swipeAngle <= 45) || 
+                        (otherDot.swipeAngle < -135 && otherDot.swipeAngle >= 135)) 
+                    { 
+                        otherDot.MakeBomb(BombType.Row);
+                    } else {
                         otherDot.MakeBomb(BombType.Column);
                     }
                 }
